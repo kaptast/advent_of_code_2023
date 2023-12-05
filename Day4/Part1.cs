e11bb48107353de68b11a42bb0d45c51fb088da8
kaptast <kaptas.tamas@gmail.com>
2023-12-06 00:08:54 +0100

    solve day4 part 1

---
+++ Day4/Part1.cs
@@ -0,0 +1,22 @@
+﻿namespace Day4;
+
+public static class Part1
+{
+    public static int ParseGames(string game)
+    {
+        var parts = game.Split(':', StringSplitOptions.TrimEntries);
+        var numberParts = parts[1].Split("|", StringSplitOptions.TrimEntries);
+
+        var winningNumbers = ParseNumbers(numberParts[0]);
+        var numberIHave = ParseNumbers(numberParts[1]);
+
+        var wonNumbers = numberIHave.Intersect(winningNumbers);
+
+        return 1 * (int)Math.Pow(2, wonNumbers.Count() - 1);
+    }
+
+    public static List<int> ParseNumbers(string winningNumbers)
+    {
+        return winningNumbers.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,20 @@
+﻿using Day4;
+
+int SummarizeLines(Func<string, int> solver)
+{
+    var lines = File.ReadAllLines("input.txt");
+
+    var sum = 0;
+
+    foreach (var line in lines)
+    {
+        sum += solver(line);
+    }
+
+    return sum;
+}
+
+Console.WriteLine(SummarizeLines(Part1.ParseGames));
+
+
+Console.WriteLine(Part2.ParseGames());
