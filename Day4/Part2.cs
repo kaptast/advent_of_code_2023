dd69d6c317a2c04b938d5a0c7ac904f82426c3b8
kaptast <kaptas.tamas@gmail.com>
2023-12-06 19:52:16 +0100

    not working attempt at day4 part 2

---
+++ Day4/Part2.cs
@@ -0,0 +1,46 @@
+﻿namespace Day4;
+
+public static class Part2
+{
+    public static int ParseGames()
+    {
+        var lines = File.ReadAllLines("input.txt").ToList();
+
+        for (var i = 0; i < lines.Count; i++)
+        {
+            var result = ParseGame(lines[i]);
+
+            var linesToAdd = new List<string>();
+            for (var j = 1; j <= result; j++)
+            {
+                var index = 2 * j + i;
+                linesToAdd.Add(lines[index]);
+            }
+
+            for (var j = 1; j <= result; j++)
+            {
+                lines.Insert(2 * j + i, linesToAdd[j - 1]);
+            }
+        }
+
+        return lines.Count;
+    }
+
+    public static int ParseGame(string game)
+    {
+        var parts = game.Split(':', StringSplitOptions.TrimEntries);
+        var numberParts = parts[1].Split("|", StringSplitOptions.TrimEntries);
+
+        var winningNumbers = ParseNumbers(numberParts[0]);
+        var numberIHave = ParseNumbers(numberParts[1]);
+
+        var wonNumbers = numberIHave.Intersect(winningNumbers);
+
+        return wonNumbers.Count();
+    }
+
+    public static List<int> ParseNumbers(string winningNumbers)
+    {
+        return winningNumbers.Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+    }
+}
