db1b919a2badf97d75272f64fe83d138a8da04d3
kaptast <kaptas.tamas@gmail.com>
2023-12-06 23:35:06 +0100

    solve day6

---
+++ Day6/Part1.cs
@@ -0,0 +1,40 @@
+﻿namespace Day6;
+
+public static class Part1
+{
+    public static int Solve(string[] lines)
+    {
+        var times = ReadLine(lines[0]);
+        var records = ReadLine(lines[1]);
+
+        var allTheSolutions = 1;
+
+        for (int i = 0; i < times.Count; i++)
+        {
+            var solutions = 0;
+
+            var time = times[i];
+            var record = records[i];
+
+            for (int holdTime = 1; holdTime <= time; holdTime++)
+            {
+                var distance = (time - holdTime) * holdTime;
+                if (distance > record)
+                {
+                    solutions++;
+                }
+            }
+
+            allTheSolutions *= solutions;
+        }
+
+        return allTheSolutions;
+    }
+
+    public static List<int> ReadLine(string line)
+    {
+        var parts = line.Split(':', StringSplitOptions.TrimEntries);
+
+        return parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+    }
+}
---
+++ Day6/Part2.cs
@@ -0,0 +1,30 @@
+﻿namespace Day6;
+
+public static class Part2
+{
+    public static long Solve(string[] lines)
+    {
+        var time = ReadLine(lines[0]);
+        var record = ReadLine(lines[1]);
+
+        var solutions = 0;
+
+        for (long holdTime = 1; holdTime <= time; holdTime++)
+        {
+            var distance = (time - holdTime) * holdTime;
+            if (distance > record)
+            {
+                solutions++;
+            }
+        }
+
+        return solutions;
+    }
+
+    public static long ReadLine(string line)
+    {
+        var parts = line.Split(':', StringSplitOptions.TrimEntries);
+
+        return long.Parse(parts[1].Replace(" ", ""));
+    }
+}
